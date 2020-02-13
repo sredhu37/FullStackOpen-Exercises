1d90339f21a0f561150ddd89d220490645914238
Sunny Redhu <sunny.redhu@o9solutions.com>
2020-02-13 19:26:44 +0530

    2D completed

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import Filter from './Filter';
 import PersonForm from './PersonForm';
 import Persons from './Persons';
+import personsService from '../services/persons';
 
 const App = () => {
     const [ persons, setPersons] = useState([]);
@@ -13,15 +13,15 @@ const App = () => {
 
     const fetchDataFromServer = () => {
         console.log('Before fetching data from server');
-        axios
-        .get('http://localhost:3001/persons')
-        .then((response) => {
+        personsService
+            .getAll()
+            .then((response) => {
             console.log(`Got response: ${response.data}`);
             setPersons(response.data);
-        })
-        .catch((error) => {
-            console.log(`Cannot fetch data from server: ${error}`);
-        });
+            })
+            .catch((error) => {
+                console.log(`Cannot fetch data from server: ${error}`);
+            });
         console.log('Before fetching data from server');
     }
 
@@ -37,13 +37,36 @@ const App = () => {
                 number: newNumber
             };
 
-            setPersons(persons.concat(newPerson));
-            setNewName('');
-            setNewNumber('0000000000');
-
-            // updateSearchResults();
+            personsService
+                .create(newPerson)
+                .then(response => {
+                    setPersons(persons.concat(response.data));
+                    setNewName('');
+                    setNewNumber('0000000000');
+                })
+                .catch(error => {
+                    console.log(`Error while sending new user to server: ${error}`);
+                });
         } else {
-            alert(`${newName} is already added to phonebook`);
+            const result = window.confirm(`newName is already added to phonebook, replace the old number with a new one?`);
+            if(result) {
+                const existingPerson = persons.find(person => person.name === newName);
+                const newPersonInfo = { ...existingPerson, number: newNumber }
+
+                personsService
+                    .update(existingPerson.id, newPersonInfo)
+                    .then(response => {
+                        setPersons(persons.map(person => {
+                            if(person.id === existingPerson.id) {
+                                person = response.data;
+                            }
+                            return person;
+                        }));
+                    })
+                    .catch(error => {
+                        console.log(`Error in updating the number for ${existingPerson.name}`);
+                    })
+            }
         }
     }
 
@@ -57,17 +80,27 @@ const App = () => {
         setNewNumber(event.target.value);
     }
 
+    const deletePerson = (id, name) => {
+        const result = window.confirm(`Delete ${name}`);
+        if(result) {
+            personsService
+            .deleteOne(id)
+            .then(response => {
+                setPersons(persons.filter(person => person.id !== id));
+            })
+            .catch(error => console.log(`Error in deleting the person: ${error}`));
+        }
+    }
+
     const updateSearchResults = () => {
         console.log(`Type of persons: ${persons}`);
         const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(searchName.toLowerCase()));
-        return filteredPersons.map(person => (<p key={person.id}>{person.name} {person.number}</p>));
+        return filteredPersons.map(person => (<p key={person.id}>{person.name} {person.number} <button onClick={() => deletePerson(person.id, person.name)}>delete</button></p>));
     }
 
     const handleSearchNameChange = (event) => {
         console.log(event.target.value);
         setNewSearchName(event.target.value);
-
-        // updateSearchResults();
     }
 
     return (
